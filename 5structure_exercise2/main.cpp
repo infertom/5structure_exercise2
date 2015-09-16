a5a6e5f49fecdfb34f5980f4540fcc0320468007
Infertom <infertom@sina.cn>
2015-09-16 23:42:29 +0800

    删除大文件

--- 5structure_exercise2/main.cpp
+++ 5structure_exercise2/main.cpp
@@ -82,7 +82,7 @@ int main (int argc, char *argv[]) {
   /*
    * Bind to the socket
    */
-  bind (listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
+  ::bind (listenfd, (struct sockaddr *)&servaddr, sizeof(servaddr));
 
 
   /*
